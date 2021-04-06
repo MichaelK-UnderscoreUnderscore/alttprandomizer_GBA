228ce2340506f47766a04f5d3ff05066f4a5bdfb
Michael Kunze <erzengelmichael@live.de>
2021-04-06 23:02:23 +0200

    Setting up for assembly dev
    Gonna use armips to use ARM7TDMI Assembly (GBA Assembly) and FLoating IPS for patch generation I guess.
    
    build.bat to build everything. Might build everything in a way to keep supporting both US and JP actually.
    
    The 2 should be close enough for me to be able to do so, especially with all the free space I'd be able to get from removing all of 4Swords, who'd need that anyway.

---
+++ ALttP GBA Rando asm/build.asm
@@ -0,0 +1,4 @@
+.gba
+.Open "base/ALttP_US.gba","ALttP_US_MOD.gba",0
+.include "asm/intro_skip.asm"
+.Close
---
+++ ALttP GBA Rando asm/build.bat
@@ -0,0 +1,3 @@
+armips build.asm
+flips --create base/ALttP_US.gba ALttP_US_MOD.gba output/ALttP_US_MOD.bps
+pause
